d7bf888534b87ad72539c2532b97f431b229baca
Mike Feller <41796396+MikeF-Code@users.noreply.github.com>
2019-01-27 17:59:49 -0500

    Added API route for looking up/creating a new User.  Setup initial handlebars templates without styling.  Created functions for interaction with page elements to create a user/load an existing user, to perform game searches, and to show the results of that search in a formatted table.

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,92 @@
-const axios = require("axios");
+let userId;
+
+// Prompt the user for their username upon page load.
+$(document).ready(function() {
+    // Initialize Materialize modal
+    // $('.modal').modal();
+    usernameSelect();
+    gameSearch();
+});
+
+
+    // Display modal for user selection
+    // $('#userSelection').modal('open');
+
+
+// On clicking the Submit button, this will run the API route for user login/creation.  Returned object will be used to populate #username, as well as store the userId global variable for later use.
+function usernameSelect() {
+    $('#usernameInput').on("click", function(event){
+            // Pull username string out of text input
+            let userCheck = $('#usernameText').val().trim();
+            // Hit API and run the route for user login/creation
+            $.ajax('/api/user/'+userCheck)
+                // What to do with promise
+                .then(
+                    function(result) {
+                        console.log("Result of usernameSelect():");
+                        // Sequelize's .findOrCreate() method returns results in an array, so we must select the first index value to get actual user data.
+                        console.log(result[0]);
+                        // Map the primary key id value of the returned user to the global userId variable, for later queries.
+                        userId = result[0].id;
+                        console.log("userId is "+userId);
+                        // Display the returned username in the appropriate div element.
+                        let usernameDiv = $('#username');
+                        usernameDiv.append(result[0].username);
+                    }
+                );
+        });
+}
+
+// Upon pressing the enter key in the search box, query the Giant Bomb API using the game name specified by the user in the search box.  We'll use the query results to build the table of search results.
+function gameSearch() {
+    // Keypress event targeted to the search box
+    $('#search').keypress(function(event){
+        // Checks to ensure Enter keypress
+        if(event.which == 13) {
+            // Map the text written in search box to variable gameTitle
+            let gameTitle = $('#search').val().trim();
+            // Concatenate the gameTitle into the queryURL for AJAX use.
+            let queryURL = 'https://www.giantbomb.com/api/search/?api_key=b380d9444fd2c487e9c742b033394e9db0e56d68&query='+gameTitle+'&limit=10&resources=game&format=json&field_list=name,guid,image,platforms'
+            // Run AJAX query then perform some logic with the response.
+            $.ajax(queryURL).then(function(response){
+                console.log("Name is: "+response.results[0].name);
+                console.log("Boxart URL is: "+response.results[0].image.icon_url);
+                console.log("First platform listed is: "+response.results[0].platforms[0].abbreviation);
+                console.log("Guid is: "+response.results[0].guid);
+                
+                let resultsTableDiv = $('#searchResults');
+                let resultsTableMain = $('<table>');
+                let resultsTableBody = $('<tbody>');
+                resultsTableDiv.append(resultsTableMain);
+                resultsTableDiv.append(resultsTableBody);
+                for (let i = 0; i < response.results.length; i++) {
+                    let resultsTableRow = $('<tr>');
+                    resultsTableDiv.append(resultsTableRow);
+                    let resultsTableBoxartDisplay = $('<td>');
+                    let resultsTableBoxart = $('<img>');
+                    resultsTableBoxart.attr('src', response.results[i].image.icon_url);
+                    resultsTableBoxartDisplay.append(resultsTableBoxart);
+                    resultsTableDiv.append(resultsTableBoxartDisplay);
+                    let resultsTableNameDisplay = $('<td>');
+                    let resultsTableName = response.results[i].name;
+                    resultsTableNameDisplay.append(resultsTableName);
+                    resultsTableDiv.append(resultsTableNameDisplay);
+                    let resultsTablePlatformsDisplay = $('<td>');
+                    let resultsTablePlatforms;
+                    for (let j = 0; j < response.results[i].platforms.length; j++) {
+                        let platformLoop = response.results[i].plaforms[j].abbreviation;
+                        resultsTablePlatforms.append(platformLoop);
+                    }
+                    resultsTablePlatformsDisplay.append(resultsTablePlatforms);
+                    resultsTableDiv.append(resultsTablePlatformsDisplay);
+                    let resultsTableButtonDisplay = $('<td>');
+                    let resultsTableButton = $('<a>');
+                    resultsTableButton.attr("class", "btn-floating btn-small waves-effect waves-light grey darken-1");
+                    resultsTableButton.attr("id", "addToCollection");
+                    resultsTableButtonDisplay.append(resultsTableButton);
+                    resultsTableDiv.append(resultsTableButtonDisplay);
+                }
+            });
+        }
+    });
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,9 @@
 const db = require('../models');
 
 module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.render("index");
+    });
     // Route for pulling collection
     app.get("/api/collection/:id", function(req, res) {
         let userId = req.params.id;
@@ -21,11 +24,27 @@ module.exports = function(app) {
         let gameId = req.params.gameId;
         db.User.findById(userId).then(function(user){
             db.Game.findById(gameId).then(function(game){
-                user.addGame(game).then(function(dbusergame) {
-                    res.json(dbusergame);
-                    console.log(dbusergame);
+                user.addGame(game).then(function(dbGame) {
+                    res.json(dbGame);
+                    console.log(dbGame);
                 });
             });
+        });
+    });
+    // Route for a user login/creation.
+    app.get("/api/user/:username", function(req, res) {
+        let userName = req.params.username;
+        db.User.findOrCreate({
+            where: {
+                username: userName
+            }
+        }).then(function(user){
+            let handlebarsObj = {
+                username: user
+            };
+            console.log(user);
+            res.json(user);
+            // res.render("index", handlebarsObj);
         });
     });
 };
